a36cb827061bef36131d8106e1ab0701987e634f
Phan Huu Viet <phanhuuviet1@gmail.com>
2025-02-26 23:08:19 +0700

    update/app-module: add ssl ca connect db stg

--- seed.ts
+++ seed.ts
@@ -11,6 +11,36 @@ const pool = new Pool({
   user: process.env.DATABASE_POSTGRES_USERNAME,
   password: process.env.DATABASE_POSTGRES_PASSWORD,
   database: process.env.DATABASE_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: `-----BEGIN CERTIFICATE-----
+MIIETTCCArWgAwIBAgIUUjFLGjsKmL54hQaLufT65gfYk9swDQYJKoZIhvcNAQEM
+BQAwQDE+MDwGA1UEAww1YzM5MDE3ZjMtNmMxZi00NTZlLWEyM2EtZDRkYThhM2U5
+YTIyIEdFTiAxIFByb2plY3QgQ0EwHhcNMjUwMjI2MTU1NDU4WhcNMzUwMjI0MTU1
+NDU4WjBAMT4wPAYDVQQDDDVjMzkwMTdmMy02YzFmLTQ1NmUtYTIzYS1kNGRhOGEz
+ZTlhMjIgR0VOIDEgUHJvamVjdCBDQTCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCC
+AYoCggGBALYTJKzq+23+a6AS9IstCvZrPdxstz+oQUjvsYdTncuTkPfRc8/IJ1hO
+V5N6qZrg80WPx9QcbxAmhlY/fk/5CI9mEXIBl+4hd4q+Gt/1NGIFQqBZaXNHZLPE
+IgSSIVMPFDZuxSqpv0KFa3lfWENIKbDmsi1t2ZEzGVkUPqdeaUz76a878l/fUR6o
+CwzY9P27qh8GAtRn2o/QXAn7oNHqiJUGReElHFmpTJq690x8mPVMcZHBTbJJO8mA
+OWEbAQ/LacHy7wPHhhtKhfpuftDJXQImSfDA59G4PhPWvwfatDAM3zbXTMCROUVE
+CZOWgjQgb5XN8/8fjCIK6tdB6SK4La0AAPpfioi5RnDsm5DIcYu3daid9kKWR81h
+/h31pBh/r/CCBo3JTbf75884khiuzMFqzAy7eKFt3BKmLcENUPM7PP0nak1nd+ay
+UVMnei65wmb7BxleZUFu1rbFSQ+kkK/3lmb8FvNgWNJrI4NYoqFz6aXUeVFPXM0r
+AzrusJgrbQIDAQABoz8wPTAdBgNVHQ4EFgQU3UWj6vW9przFlrUlixgwIGkmfD4w
+DwYDVR0TBAgwBgEB/wIBADALBgNVHQ8EBAMCAQYwDQYJKoZIhvcNAQEMBQADggGB
+ABQNeJO+7o6bcg4EG4BuUrfwkwLI8e4iNzh7Df4fRuwGe8SeDI1YsrgQ+mA0kZbc
+z+E0mZjanqO1tHfpBO2oKl3Yxryz+lHuHxGOTFgJWfQEenjcBHUoElgjtxsjM9Us
+3rO8QI9n9QTlBxbErUlodBRzjfRMDwMqsWxk+qBH3S6cWcsMaYR+my/Xz/h+y5rC
+qAMbe5MJxwm/7JJ9REikuR8HO87vbPwij0uXB1kpb9fC7lnBUixxXxJcAwUwQEkp
+rSIgTI8+yMpvv3OtcWZwNEpv+g/m/st5E6tfc1NAErNLo/6TiaNBuU4yC2FRVnVv
+25vCh8vDrOQYYcXxkLDgwI/HGzt+ppRE2DMUDbUiHYLGpPglJPhX7Jg64aeHF+rE
+B7iP659vWNPpmgKl24muavP5R7jckLU7p99k6K+ABAZY6BygP3ObuWt5JzHaiK/W
+HRkkAjKPgOk7OyS9PeUatBHBqZzGkF8ESUs84HCe4kloENhXaxnDb8JyizTLv4w6
+Mg==
+-----END CERTIFICATE-----
+`,
+},
 });
 
 // Kết nối cơ sở dữ liệu
@@ -22,7 +52,7 @@ pool.connect((err, client, release) => {
   console.log('Đã kết nối thành công đến cơ sở dữ liệu PostgreSQL.');
 
   // Thực hiện các truy vấn cơ sở dữ liệu
-  client.query(sql, (err, result) => {
+  client.query(sql, (err) => {
     release(); // Giải phóng kết nối
 
     if (err) {
